9a17a852b6e01b5b694e98ba8b5896708b1ae633
Michael Adelson <mike.adelson314@gmail.com>
2014-09-13 12:42:15 -0400

    More sql tests

--- MedallionOData.Tests/Service/Sql/SqlTestBase.cs
+++ MedallionOData.Tests/Service/Sql/SqlTestBase.cs
@@ -12,19 +12,10 @@ namespace Medallion.OData.Tests.Service.Sql
 {
     public abstract class SqlTestBase
     {
-        [ClassInitialize]
-        public static void CreateDatabase()
-        {
-            using (var context = new CustomersContext())
-            {
-                context.Customers.Any().ShouldEqual(true);
-            }
-        }
-
         protected abstract ODataSqlContext Context { get; }
 
         [TestMethod]
-        public void SqlReadNames()
+        public void SqlSimpleSelect()
         {
             var names = this.Context.Query<Customer>("customers").Select(c => c.Name).ToArray();
             var dynamicNames = this.Context.Query<ODataEntity>("customers").Select(c => c.Get<string>("Name")).ToArray();
@@ -48,6 +39,49 @@ namespace Medallion.OData.Tests.Service.Sql
                 var expected = context.Customers.Where(c => c.Name.Contains("b") || c.Salary > 100);
                 filtered.CollectionShouldEqual(expected);
                 dynamicFiltered.Select(c => c.Get<Guid>("Id")).CollectionShouldEqual(expected.Select(c => c.Id));
+            }
+        }
+
+        [TestMethod]
+        public void SqlSort()
+        {
+            var sorted = this.Context.Query<Customer>("customers")
+                .OrderBy(c => c.Salary)
+                .ThenBy(c => c.Name.Length)
+                .ThenByDescending(c => c.Name)
+                .ToArray();
+            var dynamicSorted = this.Context.Query<ODataEntity>("customers")
+                .OrderBy(c => c.Get<double>("Salary"))
+                .ThenBy(c => c.Get<string>("Name").Length)
+                .ThenByDescending(c => c.Get<string>("Name"))
+                .ToArray();
+            using (var context = new CustomersContext())
+            {
+                var expected = context.Customers.OrderBy(c => c.Salary)
+                    .ThenBy(c => c.Name.Length)
+                    .ThenByDescending(c => c.Name)
+                    .ToArray();
+                sorted.CollectionShouldEqual(expected, orderMatters: true);
+                dynamicSorted.Select(c => c.Get<string>("Name")).CollectionShouldEqual(expected.Select(c => c.Name), orderMatters: true);
+            }
+        }
+
+        [TestMethod]
+        public void SqlPaginate()
+        {
+            var sorted = this.Context.Query<Customer>("customers").OrderByDescending(c => c.Name.Length)
+                .ThenBy(c => c.Name);
+            var dynamicSorted = this.Context.Query<ODataEntity>("customers").OrderByDescending(c => c.Get<string>("Name").Length)
+                .ThenBy(c => c.Get<string>("Name"))
+                .Select(c => c.Get<Guid>("Id"));
+            using (var context = new CustomersContext())
+            {
+                var expected = context.Customers.OrderByDescending(c => c.Name.Length)
+                    .ThenBy(c => c.Name);
+                sorted.Skip(3).Take(1).CollectionShouldEqual(expected.Skip(3).Take(1));
+                dynamicSorted.Skip(1).Take(3).CollectionShouldEqual(expected.Select(c => c.Id).Skip(1).Take(3), orderMatters: true);
+                sorted.Take(2).CollectionShouldEqual(expected.Take(2), orderMatters: true);
+                dynamicSorted.Skip(2).CollectionShouldEqual(expected.Select(c => c.Id).Skip(2), orderMatters: true);
             }
         }
     }
