6facb504ab9b65516648634b4b4c8f044ee6a664
Michael Adelson <mike.adelson314@gmail.com>
2014-09-21 20:02:28 -0400

    DataTables example

--- MedallionOData.Samples.Web/Controllers/ODataMvcController.cs
+++ MedallionOData.Samples.Web/Controllers/ODataMvcController.cs
@@ -1,7 +1,11 @@
-﻿using Medallion.OData.Service;
+﻿using Medallion.OData.Client;
+using Medallion.OData.Service;
+using Medallion.OData.Service.Sql;
+using Medallion.OData.Tests.Integration;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
@@ -18,6 +22,65 @@ namespace Medallion.OData.Samples.Web.Controllers
             var service = new ODataService();
             var result = service.Execute(Company.GetCompanies(), HttpUtility.ParseQueryString(this.Request.Url.Query));
             return this.Content(result.Results.ToString(), "application/json");
+        }
+
+        [HttpGet]
+        public ActionResult DataTable()
+        {
+            return this.View("DataTable");
+        }
+
+        [HttpGet]
+        public ActionResult Data(int id)
+        {
+            using (var context = new CustomersContext()) 
+            {
+                context.Database.Initialize(force: false);
+
+                var schema = GenerateRandomSchema(id);
+                var sql = string.Join(
+                    Environment.NewLine + "UNION ALL ",
+                    Enumerable.Range(0, count: schema.Values.First().Count)
+                        .Select(i => "SELECT " + string.Join(", ", schema.Select(kvp => string.Format("'{0}' AS {1}", kvp.Value[i], kvp.Key))))
+                );
+
+                var sqlContext = new ODataSqlContext(
+                    new SqlServerSyntax(SqlServerSyntax.Version.Sql2012), 
+                    new DefaultSqlExecutor(() => new SqlConnection(context.Database.Connection.ConnectionString))
+                );
+                var query = sqlContext.Query<ODataEntity>("(" + sql + ")");
+
+                var service = new ODataService();
+                // hack on OData v4 handling
+                var queryParameters = HttpUtility.ParseQueryString(this.Request.Url.Query);
+                if (bool.Parse(queryParameters["$count"] ?? bool.FalseString)) 
+                {
+                    queryParameters["$inlinecount"] = "allpages";
+                }
+                var result = service.Execute(query, queryParameters);
+                var resultJObject = JObject.Parse(result.Results.ToString());
+                JToken count;
+                if (resultJObject.TryGetValue("odata.count", out count))
+                {
+                    resultJObject["@odata.count"] = count;
+                }
+                return this.Content(resultJObject.ToString(), "application/json");
+            }
+        }
+
+        public static IReadOnlyDictionary<string, IReadOnlyList<string>> GenerateRandomSchema(int seed)
+        {
+            var alphabet = string.Join(string.Empty, Enumerable.Range('a', 26).Select(i => (char)i));
+
+            var random = new Random(seed);
+            var rowCount = random.Next(20, 65);
+            var result = Enumerable.Range(0, random.Next(3, 8))
+                .Select(i => new { index = i, word = string.Join(string.Empty, Enumerable.Range(0, random.Next(1, 8)).Select(_ => alphabet[random.Next(0, alphabet.Length)])) })
+                .ToDictionary(
+                    t => "col_" + t.index + "_" + t.word,
+                    t => (IReadOnlyList<string>)Enumerable.Range(0, rowCount).Select(i => string.Join(string.Empty, t.word.OrderBy(_ => random.Next()))).ToArray()
+                );
+            return result;
         }
 	}
 
---
+++ MedallionOData.Samples.Web/Scripts/jquery.dataTables.odata.js
@@ -0,0 +1,182 @@
+﻿/**
+ * @summary     DataTables OData addon
+ * @description Enables jQuery DataTables plugin to read data from OData service.
+ * @version     1.0.4
+ * @file        jquery.dataTables.odata.js
+ * @authors     Jovan & Vida Popovic
+ *
+ * @copyright Copyright 2014 Jovan & Vida Popovic, all rights reserved.
+ *
+ * This source file is free software, under either the GPL v2 license or a
+ * BSD style license, available at:
+ *   http://datatables.net/license_gpl2
+ *   http://datatables.net/license_bsd
+ * 
+ * This source file is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the license files for details.
+ * 
+ */
+
+function fnServerOData(sUrl, aoData, fnCallback, oSettings) {
+
+    var oParams = {};
+    $.each(aoData, function (i, value) {
+        oParams[value.name] = value.value;
+    });
+
+    var data = {
+        "$format": "json"
+    };
+
+    // If OData service is placed on the another domain use JSONP.
+    var bJSONP = oSettings.oInit.bUseODataViaJSONP;
+
+    if (bJSONP) {
+        data.$callback = "odatatable_" + (oSettings.oFeatures.bServerSide ? oParams.sEcho : ("load_" + Math.floor((Math.random() * 1000) + 1)));
+    }
+
+    $.each(oSettings.aoColumns, function (i, value) {
+        var sFieldName = (value.sName !== null && value.sName !== "") ? value.sName : ((typeof value.mData === 'string') ? value.mData : null);
+        if (sFieldName === null || !isNaN(Number(sFieldName))) {
+            sFieldName = value.sTitle;
+        }
+        if (sFieldName === null || !isNaN(Number(sFieldName))) {
+            return;
+        }
+        if (data.$select == null) {
+            data.$select = sFieldName;
+        } else {
+            data.$select += "," + sFieldName;
+        }
+    });
+
+    if (oSettings.oFeatures.bServerSide) {
+
+        data.$skip = oSettings._iDisplayStart;
+        if (oSettings._iDisplayLength > -1) {
+            data.$top = oSettings._iDisplayLength;
+        }
+
+        // OData versions prior to v4 used $inlinecount=allpages; but v4 is uses $count=true
+        if (oSettings.oInit.iODataVersion !== null && oSettings.oInit.iODataVersion < 4) {
+            data.$inlinecount = "allpages";
+        } else {
+            data.$count = true;
+        }
+
+        var asFilters = [];
+        var asColumnFilters = []; //used for jquery.dataTables.columnFilter.js
+        $.each(oSettings.aoColumns,
+            function (i, value) {
+
+                var sFieldName = value.sName || value.mData;
+                var columnFilter = oParams["sSearch_" + i]; //fortunately columnFilter's _number matches the index of aoColumns
+
+                if ((oParams.sSearch !== null && oParams.sSearch !== "" || columnFilter !== null && columnFilter !== "") && value.bSearchable) {
+                    switch (value.sType) {
+                    case 'string':
+                    case 'html':
+
+                        if (oParams.sSearch !== null && oParams.sSearch !== "")
+                        {
+                            // asFilters.push("substringof('" + oParams.sSearch + "', " + sFieldName + ")");
+                            // substringof does not work in v4???
+                            asFilters.push("indexof(tolower(" + sFieldName + "), '" + oParams.sSearch.toLowerCase() + "') gt -1");
+                        }
+
+                        if (columnFilter !== null && columnFilter !== "") {
+                            asColumnFilters.push("indexof(tolower(" + sFieldName + "), '" + columnFilter.toLowerCase() + "') gt -1");
+                        }
+                        break;
+
+                    case 'date':
+                    case 'numeric':
+                        var fnFormatValue = 
+                            (value.sType == 'numeric') ? 
+                                function(val) { return val; } :
+                                function(val) { 
+                                        // Here is a mess. OData V2, V3, and V4 se different formats of DateTime literals.
+                                        switch(oSettings.oInit.iODataVersion){
+                                                // V2 works with the following format:
+                                                // http://services.odata.org/V2/OData/OData.svc/Products?$filter=(ReleaseDate+lt+2014-04-29T09:00:00.000Z)                                                              
+                                                case 4: return (new Date(val)).toISOString(); 
+                                                // V3 works with the following format:
+                                                // http://services.odata.org/V3/OData/OData.svc/Products?$filter=(ReleaseDate+lt+datetimeoffset'2008-01-01T07:00:00')
+                                                case 3: return "datetimeoffset'" + (new Date(val)).toISOString() + "'";  
+                                                // V2 works with the following format:
+                                                // http://services.odata.org/V2/OData/OData.svc/Products?$filter=(ReleaseDate+lt+DateTime'2014-04-29T09:00:00.000Z')
+                                                case 2: return "DateTime'" + (new Date(val)).toISOString() + "'"; 
+                                        }
+                                }
+
+                        // Currently, we cannot use global search for date and numeric fields (exception on the OData service side)
+                        // However, individual column filters are supported in form lower~upper
+                        if (columnFilter !== null && columnFilter !== "" && columnFilter !== "~") {
+                            asRanges = columnFilter.split("~");
+                            if (asRanges[0] !== "") {
+                                asColumnFilters.push("(" + sFieldName + " gt " + fnFormatValue(asRanges[0]) + ")");
+                            }
+
+                            if (asRanges[1] !== "") {
+                                asColumnFilters.push("(" + sFieldName + " lt " + fnFormatValue(asRanges[1]) + ")");
+                            }
+                        }
+                        break;
+                    default:
+                    }
+                }
+            });
+
+        if (asFilters.length > 0) {
+            data.$filter = asFilters.join(" or ");
+        }
+
+        if (asColumnFilters.length > 0) {
+            if (data.$filter !== undefined) {
+                data.$filter = " ( " + data.$filter + " ) and ( " + asColumnFilters.join(" and ") + " ) ";
+            } else {
+                data.$filter = asColumnFilters.join(" and ");
+            }
+        }
+
+        var asOrderBy = [];
+        for (var i = 0; i < oParams.iSortingCols; i++) {
+            asOrderBy.push(oParams["mDataProp_" + oParams["iSortCol_" + i]] + " " + (oParams["sSortDir_" + i] || ""));
+        }
+
+        if (asOrderBy.length > 0) {
+            data.$orderby = asOrderBy.join();
+        }
+    }
+    $.ajax(jQuery.extend({}, oSettings.oInit.ajax, {
+        "url": sUrl,
+        "data": data,
+        "jsonp": bJSONP,
+        "dataType": bJSONP ? "jsonp" : "json",
+        "jsonpCallback": data["$callback"],
+        "cache": false,
+        "success": function (data) {
+            var oDataSource = {};
+
+            // Probe data structures for V4, V3, and V2 versions of OData response
+            oDataSource.aaData = data.value || (data.d && data.d.results) || data.d;
+            var iCount = (data["@odata.count"]) ? data["@odata.count"] : ((data["odata.count"]) ? data["odata.count"] : ((data.__count) ? data.__count : (data.d && data.d.__count)));
+
+            if (iCount == null) {
+                if (oDataSource.aaData.length === oSettings._iDisplayLength) {
+                    oDataSource.iTotalRecords = oSettings._iDisplayStart + oSettings._iDisplayLength + 1;
+                } else {
+                    oDataSource.iTotalRecords = oSettings._iDisplayStart + oDataSource.aaData.length;
+                }
+            } else {
+                oDataSource.iTotalRecords = iCount;
+            }
+
+            oDataSource.iTotalDisplayRecords = oDataSource.iTotalRecords;
+
+            fnCallback(oDataSource);
+        }
+    }));
+
+} // end fnServerData
